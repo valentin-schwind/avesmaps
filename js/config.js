3822a1135eee0ca89ff8ae56486de17f04fdc3e9
Valentin Schwind <mail@vali.de>
2026-05-14 17:56:26 +0200

    Use third compass graphic

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ const MAP_BOUNDS = [
 ];
 const MAP_DECORATION_CONFIG = {
 	compass: {
-		src: "img/compass_2.png",
+		src: "img/compass3.png",
 		coordinates: [18, 1006],
 		size: [96, 94],
 		anchor: [96, 94],
