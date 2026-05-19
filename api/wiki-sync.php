968a37dd331113d48c6689445b48cae754701fe3
Valentin Schwind <mail@vali.de>
2026-05-19 20:51:20 +0200

    Extract shared WikiSync constants

--- api/wiki-sync.php
+++ api/wiki-sync.php
@@ -1,19 +1,14 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types=1); 
 
 require __DIR__ . '/auth.php';
+require_once __DIR__ . '/wiki-sync-lib.php';
 require_once __DIR__ . '/political-territory-lib.php';
 
-const AVESMAPS_WIKI_API_URL = 'https://de.wiki-aventurica.de/de/api.php';
-const AVESMAPS_WIKI_PAGE_BASE_URL = 'https://de.wiki-aventurica.de/wiki/';
-const AVESMAPS_WIKI_USER_AGENT = 'Avesmaps WikiSync/1.0';
-const AVESMAPS_WIKI_TITLE_BATCH_SIZE = 50;
-const AVESMAPS_WIKI_SEARCH_RESULT_LIMIT = 5;
-const AVESMAPS_WIKI_REQUEST_TIMEOUT_SECONDS = 30;
 const AVESMAPS_WIKI_FUZZY_CUTOFF = 0.82;
 const AVESMAPS_WIKI_SYNC_TYPE_LOCATION = 'location';
-const AVESMAPS_WIKI_LOCK_TTL_SECONDS = 120;
+
 const AVESMAPS_WIKI_POLITICAL_TERRITORY_SEED_PAGES = [
     'Baronie/Liste',
     "Bergk\u{00F6}nigreich/Liste",
