f57774fce4cbabf0abeb3c798516a8d21d4e68e6
Valentin Schwind <mail@vali.de>
2026-05-21 02:31:10 +0200

    Add territory WikiSync new tab override

---
+++ js/political-territory-wikisync-tab.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function openPoliticalTerritoryWikiSyncSettingsInNewTab() {
+	const settingsUrl = "/html/wiki-dom-sync-settings.html";
+	const openedWindow = window.open(settingsUrl, "_blank");
+	if (openedWindow) {
+		openedWindow.opener = null;
+		return;
+	}
+	window.location.href = settingsUrl;
+}
+
+window.startWikiSyncTerritoryRun = function startWikiSyncTerritoryRunFromNewTabOverride() {
+	if (typeof setWikiSyncStatus === "function") {
+		setWikiSyncStatus("Synchronisierungseinstellungen werden geöffnet...", "pending");
+	}
+	openPoliticalTerritoryWikiSyncSettingsInNewTab();
+};
