f4309c65875cee8320a1ac5dacb358d7fb7d480e
Valentin Schwind <mail@vali.de>
2026-05-24 16:13:15 +0200

    Split map features display mode helpers

--- js/map-features.js
+++ js/map-features.js
@@ -172,40 +172,6 @@ function isMarkerEntryInRenderBounds(entry, renderBounds = getMapRenderBounds())
 	return entry?.marker && isLatLngInRenderBounds(entry.marker.getLatLng(), renderBounds);
 }
 
-function syncPathVisibility() {
-	const showPaths = $("#togglePaths").is(":checked");
-	$.each(pathLayers, (i, layer) => {
-		const path = pathData[i];
-		const shouldShow = showPaths && shouldShowPathOnMap(path);
-		map[shouldShow ? "addLayer" : "removeLayer"](layer);
-	});
-}
-
-function shouldShowPathOnMap(path) {
-	if (IS_EDIT_MODE) {
-		return true;
-	}
-
-	return normalizePathSubtype(path?.properties?.feature_subtype || path?.properties?.name) !== "Seeweg";
-}
-
-function getSelectedMapLayerMode() {
-	return String($("#mapLayerModeSelect").val() || DEFAULT_PLANNER_STATE.mapLayerMode);
-}
-
-function setSelectedMapLayerMode(mode) {
-	const normalizedMode = ["none", "political", "deregraphic", "powerlines"].includes(mode) ? mode : DEFAULT_PLANNER_STATE.mapLayerMode;
-	$("#mapLayerModeSelect").val(normalizedMode);
-	syncTransportControl("mapLayerModeSelect");
-	if (IS_EDIT_MODE && normalizedMode === "powerlines") {
-		$("#toggleNodix").prop("checked", true);
-		syncLocationMarkerVisibility();
-	}
-	syncRegionVisibility();
-	syncLabelVisibility();
-	syncPowerlineVisibility();
-	syncPlannerStateToUrl();
-}
 
 function syncRegionVisibility() {
 	const showRegions = getSelectedMapLayerMode() === "political";
@@ -251,14 +217,6 @@ function syncRegionVisibility() {
 	}
 }
 
-function applyDisplayOptions() {
-	syncLocationToggleButtons();
-	syncLocationMarkerVisibility();
-	syncPathVisibility();
-	syncPowerlineVisibility();
-	syncRegionVisibility();
-	syncLabelVisibility();
-}
 
 $(".location-toggle").on("click", function () {
 	setVisibleLocationTypesThrough(String(this.dataset.locationType || ""), { syncUrl: true });
