2b378e9e997aa2b3eb2746a6462f2fe3c05393ea
Valentin Schwind <mail@vali.de>
2026-05-25 19:11:26 +0200

    Mark route subtype prefix normalizer version

--- api/route-response.php
+++ api/route-response.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/route-client-graph.php';
 
-const AVESMAPS_ROUTE_API_CODE_VERSION = 'client-graph-subtype-debug-2026-05-25';
+const AVESMAPS_ROUTE_API_CODE_VERSION = 'client-graph-prefix-normalizer-2026-05-25';
 
 class AvesmapsRouteLocationNotFoundException extends RuntimeException {}
 class AvesmapsRouteViaNotSupportedException extends RuntimeException {}
